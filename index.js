e73736f1ab014ebf7e177c44b0eb1d0c84b6dcb4
Mohammad Rahman <me@shadikur.com>
2023-05-17 22:30:04 +0200

    Vercel Deployment JSON added

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
+const port = process.env.PORT || 5008
 
 
 // Middleware 
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send({ "status": "API Server is running" })
+})
+
+app.listen(port, () => {
+    console.log(`Toys API server is running on ${port}`);
+})
