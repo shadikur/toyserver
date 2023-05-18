fc8624ecb8cc66af7826d065979dc8c2812e0e14
Mohammad Rahman <me@shadikur.com>
2023-05-18 12:43:05 +0200

    MongoDB integrated

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// All the required modules
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -9,6 +11,40 @@ const port = process.env.PORT || 5008
 // Middleware 
 app.use(cors());
 app.use(express.json());
+
+// Connection URI
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@toycluster.dc1enre.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+// ToyDB collection
+const toyCollection = client.db(process.env.DB_NAME).collection('toysdata');
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        //----------- Endpoint routes starts here  ----------
+
+
+        //------------- Endpoint routes ends here  ----------
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.status(200).send({ "status": "API Server is running" })
